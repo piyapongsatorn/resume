ab1fdd4091ad4ff3aea1c7c872a8ba8c815cc839
piyapongsatorn <35741114+piyapongsatorn@users.noreply.github.com>
2018-10-16 12:03:57 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require("express")
+const app = express()
+const port = 8000
+
+app.use(express.static(__dirname));
+app.get('/index.html',(req,res)=>res.sendFile(__dirname+"/"+"index.html"))
+
+app.listen(port, () => console.log('app listening on port'+port))
